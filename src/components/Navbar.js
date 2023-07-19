1eeb716a9f7c4f3ff6ea459bd2df2afe176417b3
Jack Hipperson <jackhipperson@hotmail.com>
2023-07-19 22:51:36 +0100

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div class="navbar">
+            <h1>NoteKeeper</h1>
+        </div>
+    )
+}
+
+export default Navbar;
